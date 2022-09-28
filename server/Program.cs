a1ea5016f83ace973e85f7f4f5370a6c950b11a1
Alexey <aleksei.nagaev@onlyoffice.com>
2022-09-28 18:58:23 +0300

    update example

--- server/Program.cs
+++ server/Program.cs
@@ -1,15 +1,18 @@
-var builder = WebApplication.CreateBuilder(new WebApplicationOptions
-{
-	// folder with content
-	WebRootPath = "wwwroot"
-});
+// TODO config file
+
+var curr_dir = Directory.GetCurrentDirectory();
+var web_root_dir = "wwwroot";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// builder configuration
+builder.Environment.WebRootPath = Path.Combine(curr_dir, web_root_dir);
 var app = builder.Build();
-
-
-app.UseStaticFiles();
 
 // redirect to index.html or default.html
 app.UseDefaultFiles();
+
+app.UseStaticFiles();
 
 // simple example to request
 app.MapGet("/api", () => "Example API");
