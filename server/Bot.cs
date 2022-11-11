37fa07518cfd621682924412a2c1652648bfeee1
Alexey <aleksei.nagaev@onlyoffice.com>
2022-11-11 16:37:43 +0300

    dotnet 7 migration

---
+++ server/Bot.cs
@@ -0,0 +1,11 @@
+namespace Bot
+{
+	public static class BotExtensions
+	{
+		public static async Task<IResult> MapBot(HttpContext context)
+		{
+			// bot code here.
+			return Results.Ok();
+		}
+	}
+}
