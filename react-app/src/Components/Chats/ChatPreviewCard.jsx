90618349e1a4629d3b0424662f1feeb7c7f48f4f
Aleksandr <aleksandr.nagaev@onlyoffice.com>
2022-11-15 07:40:50 +0300

    Chats page basis created

---
+++ react-app/src/Components/Chats/ChatPreviewCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChatPreviewCard(props) {
+  return (
+    <div>ChatPreview</div>
+  );
+}
+
+export default ChatPreviewCard;
--- react-app/src/Pages/Chats.jsx
+++ react-app/src/Pages/Chats.jsx
@@ -1,12 +1,77 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
+
 import NavigationBar from '../Components/NavigationBar/NavigationBar';
+import ChatPreviewCard from '../Components/Chats/ChatPreviewCard';
+// Для тестирования fetch
+const testUrl = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+// Тестирование UI
+const testItems = [
+	{
+		id: 0,
+		name: 'Чат номер один',
+		countMembers: 12,
+	},
+	{
+		id: 1,
+		name: 'Чат номер два',
+		countMembers: 13,
+	},
+	{
+		id: 2,
+		name: 'Чат номер три',
+		countMembers: 14,
+	},
+];
 
 function Chats() {
-  return (
-	<div>
-		<NavigationBar/>
-		<div>Chats here.</div>
-	</div>
-  	);
+	const [items, setItems] = useState([]);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [error, setError] = useState();
+	// Как Леха (который снюсоед) сделает бэк - поменять
+	useEffect(() => {
+		fetch(testUrl)
+		.then(res => res.json())
+		.then((result) => {
+			setItems(result);
+			setIsLoaded(true);
+		},
+		(error) => {
+			setError(error);
+			setIsLoaded(false);
+		});
+	});
+	if (error) {
+		return (
+		<>
+			<NavigationBar/>
+			Error: {error.message}
+		</>
+		);
+	} else if (!isLoaded) {
+		return (
+		<>
+			<NavigationBar/>
+			Loading...
+		</>
+		);
+	} else {
+		return (
+			<>
+				<NavigationBar/>
+				<Container>
+					<Row>
+					{
+						// Не забыть сменить testItems на items
+						testItems.map((item) => (<ChatPreviewCard attr={item} key={item.id}/>))
+					}
+					</Row>
+				</Container>
+			</>
+		);
+	}
+	
 }
 
 export default Chats;
