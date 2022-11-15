d1101fafd3a33952ad12b22adafc452ba2b9b027
Aleksandr <aleksandr.nagaev@onlyoffice.com>
2022-11-15 18:54:19 +0300

    fix

--- react-app/src/Pages/Layout.jsx
+++ react-app/src/Pages/Layout.jsx
@@ -1,5 +1,5 @@
 import NavigationBar from '../Components/NavigationBar/NavigationBar';
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 
 function Layout() {
 	return (
