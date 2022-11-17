ac7bc009fd4407c1192fac9638d1e962b4284982
Alexei Koshelev <aleksei.koshelev@onlyoffice.com>
2022-11-18 01:30:39 +0300

    Corrected width content for pages signin and signup

--- react-app/src/Pages/SignIn.jsx
+++ react-app/src/Pages/SignIn.jsx
@@ -36,70 +36,72 @@ function SignIn() {
 
 	return (
     	<div>
-			<Container className='d-flex flex-column align-items-center mt-5' style={{ width: '22rem' }}>
-				<div className='d-flex flex-column align-items-center w-100 mb-3'>
-					<Link to='/'>
-						<img 
-							src ={logo}
-							height = '60'
-							width= '60'
-							className='d-inline-block align-top'
-							alt = "Logo"
-						/>
-					</Link>
-					<h1>Авторизация</h1>
-				</div>
-				{response && (
-					<Card className='w-100 mb-3' border={'danger'}>
-						<Card.Body className='d-flex justify-content-between'>
-							<span>{response}</span> 
-							<CloseButton onClick={event => setResponse('')}></CloseButton>
+			<Container className='d-flex flex-column align-items-center mt-5'>
+				<div style={{maxWidth: '325px', width: '100%'}}>
+					<div className='d-flex flex-column align-items-center w-100 mb-3'>
+						<Link to='/'>
+							<img 
+								src ={logo}
+								height = '60'
+								width= '60'
+								className='d-inline-block align-top'
+								alt = "Logo"
+							/>
+						</Link>
+						<h1>Авторизация</h1>
+					</div>
+					{response && (
+						<Card className='w-100 mb-3' border={'danger'}>
+							<Card.Body className='d-flex justify-content-between'>
+								<span>{response}</span> 
+								<CloseButton onClick={event => setResponse('')}></CloseButton>
+							</Card.Body>
+						</Card>
+					)}
+					<Card className='w-100 mb-3'>
+						<Card.Body>
+							<Form noValidate onSubmit={onSubmit}>
+								<Form.Group className='mb-3'>
+									<Form.Label>Email или имя пользователя</Form.Label>
+									<Form.Control 
+										type="text" 
+										required 
+										value={form.email} 
+										onChange={e => setInput('email', e.target.value)}
+										isInvalid={validated && errors.email}
+									/>
+									<Form.Control.Feedback type="invalid">
+										Заполните поле
+									</Form.Control.Feedback>
+								</Form.Group>
+								<Form.Group className='mb-3'>
+									<Form.Label className='d-flex justify-content-between'>
+										<span>Пароль</span>
+										<Link to='/pass_reset'><small>Забыли пароль?</small></Link>
+									</Form.Label>
+									<Form.Control
+										type="password" 
+										required 
+										value={form.password} 
+										onChange={e => setInput('password', e.target.value)}
+										isInvalid={validated && errors.password}
+									/>
+									<Form.Control.Feedback type="invalid">
+										Заполните поле 
+									</Form.Control.Feedback>
+								</Form.Group>
+								<Button variant="primary" type='submit' className='w-100'>
+									Войти
+								</Button>
+							</Form>
 						</Card.Body>
 					</Card>
-				)}
-				<Card className='w-100 mb-3'>
-					<Card.Body>
-						<Form noValidate onSubmit={onSubmit}>
-							<Form.Group className='mb-3'>
-								<Form.Label>Email или имя пользователя</Form.Label>
-								<Form.Control 
-									type="text" 
-									required 
-									value={form.email} 
-									onChange={e => setInput('email', e.target.value)}
-									isInvalid={validated && errors.email}
-								/>
-								<Form.Control.Feedback type="invalid">
-									Заполните поле
-								</Form.Control.Feedback>
-							</Form.Group>
-							<Form.Group className='mb-3'>
-								<Form.Label className='d-flex justify-content-between'>
-									<span>Пароль</span>
-									<Link to='/pass_reset'><small>Забыли пароль?</small></Link>
-								</Form.Label>
-								<Form.Control
-									type="password" 
-									required 
-									value={form.password} 
-									onChange={e => setInput('password', e.target.value)}
-									isInvalid={validated && errors.password}
-								/>
-								<Form.Control.Feedback type="invalid">
-									Заполните поле 
-								</Form.Control.Feedback>
-							</Form.Group>
-							<Button variant="primary" type='submit' className='w-100'>
-								Войти
-							</Button>
-						</Form>
-					</Card.Body>
-				</Card>
-				<Card className='w-100'>
-					<Card.Body className='text-center'>
-						<span>Нет аккаунта? <Link to='/signup'>Создать аккаунт</Link></span> 
-					</Card.Body>
-				</Card>
+					<Card className='w-100'>
+						<Card.Body className='text-center'>
+							<span>Нет аккаунта? <Link to='/signup'>Создать аккаунт</Link></span> 
+						</Card.Body>
+					</Card>
+				</div>
 			</Container>
 	 	</div>
   	);
--- react-app/src/Pages/SignUp.jsx
+++ react-app/src/Pages/SignUp.jsx
@@ -48,84 +48,86 @@ function SignUp() {
 
 	return (
     	<div>
-			<Container className='d-flex flex-column align-items-center mt-5' style={{ width: '22rem' }}>
-				<div className='d-flex flex-column align-items-center w-100 mb-3'>
-					<Link to='/'>
-						<img 
-							src ={logo}
-							height = '60'
-							width= '60'
-							className='d-inline-block align-top'
-							alt = "Logo"
-						/>
-					</Link>
-					<h1>Регистрация</h1>
-				</div>
-				{response && (
-					<Card className='w-100 mb-3' border={'danger'}>
-						<Card.Body className='d-flex justify-content-between'>
-							<span>{response}</span> 
-							<CloseButton onClick={event => setResponse('')}></CloseButton>
+			<Container className='d-flex flex-column align-items-center mt-5'>
+				<div style={{maxWidth: '325px', width: '100%'}}>
+					<div className='d-flex flex-column align-items-center w-100 mb-3'>
+						<Link to='/'>
+							<img 
+								src ={logo}
+								height = '60'
+								width= '60'
+								className='d-inline-block align-top'
+								alt = "Logo"
+							/>
+						</Link>
+						<h1>Регистрация</h1>
+					</div>
+					{response && (
+						<Card className='w-100 mb-3' border={'danger'}>
+							<Card.Body className='d-flex justify-content-between'>
+								<span>{response}</span> 
+								<CloseButton onClick={event => setResponse('')}></CloseButton>
+							</Card.Body>
+						</Card>
+					)}
+					<Card className='w-100 mb-3'>
+						<Card.Body>
+							<Form noValidate onSubmit={onSubmit}>
+								<Form.Group className='mb-3'>
+									<Form.Label>Имя пользователя</Form.Label>
+									<Form.Control 
+										type="text" 
+										required 
+										value={form.nickname} 
+										onChange={e => setInput('nickname', e.target.value)}
+										isInvalid={validated && errors.nickname}
+									/>
+									<Form.Control.Feedback type="invalid">
+										Имя должно быть длинной от 3 до 24 символов и состоять из латинских букв, цифр, символов(-._)
+									</Form.Control.Feedback>
+								</Form.Group>
+								<Form.Group className='mb-3'>
+									<Form.Label>
+										Пароль
+									</Form.Label>
+									<Form.Control
+										type="text" 
+										required 
+										value={form.password} 
+										onChange={e => setInput('password', e.target.value)}
+										isInvalid={validated && errors.password}
+									/>
+									<Form.Control.Feedback type="invalid">
+										Пароль должен быть длинной от 6 до 24 символов и состоять из латинских букв, цифр, символов(!@#$?%^&*-._)
+									</Form.Control.Feedback>
+								</Form.Group>
+								<Form.Group className='mb-3'>
+									<Form.Label>
+										Повторите пароль
+									</Form.Label>
+									<Form.Control
+										type="text" 
+										required 
+										value={form.passwordRepeat} 
+										onChange={e => setInput('passwordRepeat', e.target.value)}
+										isInvalid={validated && errors.passwordRepeat}
+									/>
+									<Form.Control.Feedback type="invalid">
+										Пароли не совпадают
+									</Form.Control.Feedback>
+								</Form.Group>
+								<Button variant="primary" type='submit' className='w-100'>
+									Зарегистрироваться
+								</Button>
+							</Form>
 						</Card.Body>
 					</Card>
-				)}
-				<Card className='w-100 mb-3'>
-					<Card.Body>
-						<Form noValidate onSubmit={onSubmit}>
-							<Form.Group className='mb-3'>
-								<Form.Label>Имя пользователя</Form.Label>
-								<Form.Control 
-									type="text" 
-									required 
-									value={form.nickname} 
-									onChange={e => setInput('nickname', e.target.value)}
-									isInvalid={validated && errors.nickname}
-								/>
-								<Form.Control.Feedback type="invalid">
-									Имя должно быть длинной от 3 до 24 символов и состоять из латинских букв, цифр, символов(-._)
-								</Form.Control.Feedback>
-							</Form.Group>
-							<Form.Group className='mb-3'>
-								<Form.Label>
-									Пароль
-								</Form.Label>
-								<Form.Control
-									type="text" 
-									required 
-									value={form.password} 
-									onChange={e => setInput('password', e.target.value)}
-									isInvalid={validated && errors.password}
-								/>
-								<Form.Control.Feedback type="invalid">
-									Пароль должен быть длинной от 6 до 24 символов и состоять из латинских букв, цифр, символов(!@#$?%^&*-._)
-								</Form.Control.Feedback>
-							</Form.Group>
-							<Form.Group className='mb-3'>
-								<Form.Label>
-									Повторите пароль
-								</Form.Label>
-								<Form.Control
-									type="text" 
-									required 
-									value={form.passwordRepeat} 
-									onChange={e => setInput('passwordRepeat', e.target.value)}
-									isInvalid={validated && errors.passwordRepeat}
-								/>
-								<Form.Control.Feedback type="invalid">
-									Пароли не совпадают
-								</Form.Control.Feedback>
-							</Form.Group>
-							<Button variant="primary" type='submit' className='w-100'>
-								Зарегистрироваться
-							</Button>
-						</Form>
-					</Card.Body>
-				</Card>
-				<Card className='w-100'>
-					<Card.Body className='text-center'>
-						<span>Есть аккаунт? <Link to='/signin'>Войти в аккаунт</Link></span> 
-					</Card.Body>
-				</Card>
+					<Card className='w-100'>
+						<Card.Body className='text-center'>
+							<span>Есть аккаунт? <Link to='/signin'>Войти в аккаунт</Link></span> 
+						</Card.Body>
+					</Card>
+				</div>
 			</Container>
 	 	</div>
   	);
