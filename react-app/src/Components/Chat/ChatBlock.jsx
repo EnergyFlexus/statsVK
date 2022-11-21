74fa3b578db26d3f832beabebac45cc678680442
Alexei Koshelev <aleksei.koshelev@onlyoffice.com>
2022-11-21 04:22:50 +0300

    Added skeleton for chat and members components

--- react-app/src/Components/Chat/ChatBlock.jsx
+++ react-app/src/Components/Chat/ChatBlock.jsx
@@ -16,6 +16,12 @@ function timeConverter(UNIX_timestamp){
     return time;
 }
 
+/*
+props:
+    showSkeleton        : bool      - Показать скелетон (Анимация прогрузки) 
+    countSkeletonItems  : number    - Количество скелетонов
+    messages            : array     - Сообщения
+*/
 function ChatBlock(props) {
     const messagesEndRef = useRef(null);
 
@@ -24,28 +30,39 @@ function ChatBlock(props) {
     }
 
     useEffect(() => {
-        scrollToBottom()
-    }, []);
+        if(!props.showSkeleton)
+            scrollToBottom()
+    }, [props.showSkeleton]);
 
-    return (
-        <div className={s.main}>
-            {props.messages.map((msg, index) => {
-                const isAuthorPrevious = index > 0 && msg.date - props.messages[index-1].date < 300 ? props.messages[index-1].user_id === msg.user_id : false;
-                return (<div className={isAuthorPrevious || index === 0 ? s.itemMarginSmall : s.itemMargin}>
-                    <ChatItem 
-                        text={msg.text}
-                        userVkId={msg.user_id}
-                        date={timeConverter(msg.date)}
-                        authorName={msg.name}
-                        avatar={msg.avatar}
-                        isOnlyText={isAuthorPrevious}
-                        key={msg.message_id}
-                    />
-                </div>
-            )})}
-            <div ref={messagesEndRef} />
-        </div>
-    )
+
+    if(props.showSkeleton) {
+        return (
+            <div className={s.main}>
+                {[...Array(props.countSkeletonItems)].map((x, i) =>
+                    <ChatItem showSkeleton={true} key={i}/>
+                )}
+            </div>  
+        )
+    } else {
+        return (
+            <div className={s.main}>
+                {props.messages.map((msg, index) => {
+                    const isAuthorPrevious = index > 0 && msg.date - props.messages[index-1].date < 300 ? props.messages[index-1].user_id === msg.user_id : false;
+                    return (<div key={msg.message_id} className={isAuthorPrevious || index === 0 ? s.itemMarginSmall : s.itemMargin}>
+                        <ChatItem 
+                            text={msg.text}
+                            userVkId={msg.user_id}
+                            date={timeConverter(msg.date)}
+                            authorName={msg.name}
+                            avatar={msg.avatar}
+                            isOnlyText={isAuthorPrevious}
+                        />
+                    </div>
+                )})}
+                <div ref={messagesEndRef} />
+            </div>
+        )
+    }
 }
 
 export default ChatBlock
--- react-app/src/Components/Members/MembersBlock.jsx
+++ react-app/src/Components/Members/MembersBlock.jsx
@@ -2,20 +2,38 @@ import React from 'react'
 import s from './MembersBlock.module.css';
 import MembersItem from './MembersItem';
 
+/*
+props:
+    showSkeleton        : bool      - Показать скелетон (Анимация прогрузки) 
+    countSkeletonItems  : number    - Количество скелетонов
+    members             : array     - Пользователи
+*/
 function MembersBlock(props) {
-    return (
-        <div className={s.main}>
-            {props.members.map((member, index) => (
-                <div className={index > 0 && s.itemMargin}>
-                    <MembersItem 
-                        name={member.name}
-                        vkId={member.vkId}
-                        avatar={member.avatar}
-                    />
-                </div>
-            ))}
-        </div>
-    )
+    if(props.showSkeleton) {
+        return (
+            <div className={s.main}>
+                {[...Array(props.countSkeletonItems)].map((x, i) =>
+                    <MembersItem showSkeleton={true} key={i}/>
+                )}
+            </div>  
+        )
+    } else {
+        return (
+            <div className={s.main}>
+                {props.members.map((member, index) => (
+                    <div className={index > 0 ? s.itemMargin : undefined} key={index}>
+                        <MembersItem 
+                            showSkeleton={props.showSkeleton}
+                            name={`${member.first_name} ${member.last_name}`}
+                            vkId={member.id}
+                            avatar={member.photo_50}
+                            messagesCount={member.messages_count}
+                        />
+                    </div>
+                ))}
+            </div>
+        )
+    }
 }
 
 export default MembersBlock
