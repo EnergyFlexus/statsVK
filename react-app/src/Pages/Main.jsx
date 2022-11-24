12f1c8d49e0f064b0c96956e8d23c42dec76da15
ColdwaveUwU <88661351+ColdwaveUwU@users.noreply.github.com>
2022-11-24 20:07:51 +0300

    pochti gotovo

--- react-app/src/Pages/Main.jsx
+++ react-app/src/Pages/Main.jsx
@@ -1,8 +1,84 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect, useState } from 'react';
 import { Container, Row, Col, Button} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Loading from '../Components/Loading';
 import wk from '../assets/icons-main/wk.png';
 function Main() {
+    const [error, setError] = useState();
+    const [items, setItems] = useState({});
+    const [isLoaded, setIsLoaded] = useState(false);
+    let footer;
+    useEffect(() => {
+        const getUrl = '/api/MessagesCountsInChatsAll?order=desc';
+        const getUsersUrl = '/api/ChatUsersAll';
+        const fetching = (async (url) => {
+			try {
+				let res = await fetch(url);
+				res = await res.json();
+                return res;
+			} catch (error) {
+				setError(error);
+			}
+		});
+        const prepareItems = (async () => {
+            let result = {
+                messages_count: 0,
+                chats_count: 0,
+                users_count: 0,
+            };
+            let items = await fetching(getUrl);
+            for(let item of items) {
+                result.messages_count += item.messages_count;
+                result.chats_count += 1;
+            }
+            let users = await fetching(getUsersUrl);
+            for(let item of users){
+                result.users_count += 1;
+            }
+            return result;
+        });
+        const messageAr = (async () => {
+            setItems(await prepareItems());
+            setIsLoaded(true);
+        });
+        
+        messageAr();
+    },[]);
+    
+    if(error) {
+		footer = <>Error: {error.message} </>
+	} else if(isLoaded) {
+		footer =  
+            <Container fluid style={{ backgroundColor:'	#282c34', color: "#FFFFFF"}}>
+                <Row style={{marginTop:'auto', backgroundColor:'#212529', minHeight:'10%'}}>
+                    <Container fluid style={{color: "#FFFFFF", height: '100%'}}>
+                        <Row>
+                            <Col className='ms-5'>
+                                <div style={{padding:'1%'}}>
+                                    Сообщений   
+                                    <p>{items.messages_count}</p> 
+                                </div>
+                            </Col>
+                            <Col className='ms-5'>
+                                <div style={{padding:'1%'}}>
+                                    Чатов
+                                    <p>{items.chats_count}</p>
+                                </div>
+                            </Col>
+                            <Col className='ms-5'>
+                                <div style={{padding:'1%'}}>
+                                    Участников
+                                    <p>{items.users_count}</p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Row>
+            </Container>
+	} else {
+		footer = <Loading/>
+	}
 	return (
         <>
             <Container fluid style={{ backgroundColor:'	#282c34', color: "#FFFFFF", height: '100%', flexGrow:'1'}}>
@@ -42,29 +118,9 @@ function Main() {
                     </Container>
                 </Row>
             </Container>
-            <Container fluid style={{ backgroundColor:'	#282c34', color: "#FFFFFF"}}>
-                <Row style={{marginTop:'auto', backgroundColor:'#212529', minHeight:'10%'}}>
-                    <Container fluid style={{color: "#FFFFFF", height: '100%'}}>
-                        <Row>
-                            <Col className='ms-5'>
-                                <div style={{padding:'7%'}}>
-                                            
-                                </div>
-                            </Col>
-                            <Col className='ms-5'>
-                                <div style={{padding:'7%'}}>
-                                            Количество бесед:
-                                </div>
-                            </Col>
-                            <Col className='ms-5'>
-                                <div style={{padding:'7%'}}>
-                                            Количество участников:
-                                </div>
-                            </Col>
-                        </Row>
-                    </Container>
-                </Row>
-            </Container>
+           <>
+              {footer}
+           </>
         </>
   	);
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
 		children: [
 			{
 				path: "/",
-   				element: <ChatsChart/>,
+   				element: <Main/>,
 			},
 			{
 				path: "chats",
